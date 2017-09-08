c4f950b3a90f4652aa2d6f4061ecfe120d5f6de3
unknown <darling.kicks@gmail.com>
2017-09-08 12:18:36 +0300

    classic quicksort with AML example

---
+++ qsort/qs23.py
@@ -0,0 +1,47 @@
+def qsort(a):
+    p = len(a)/2
+
+    if p == 0:
+        return a
+
+    l,r = [],[]
+
+    for i in range(len(a)):
+        if i == p:
+            continue
+
+        if a[i] <= a[p]:
+            l += [a[i]]
+        else:
+            r += [a[i]]
+
+    return qsort(l) + [a[p]] + qsort(r)
+
+import random
+def randperm(a):
+    l = len(a)
+    for i in range(l):
+        p = random.randint(0,l-1)
+        a[i], a[p] = a[p], a[i]
+
+    return a
+
+A = randperm(range(20))
+
+print A
+print qsort(A)
+# AML syntax example
+# if a[i] <= a[p]:
+#     l += [a[i]]
+# else:
+#     r += [a[i]]
+#
+# i != p:
+#     m = choose{l,r}[! (a[i] <= a[p])]
+#     m += [a[i]]
+
+#
+# if we write    m = choose{l,r}[! (a[i] <= a[p])]
+# it's equal     m = ref choose{l,r}[! (a[i] <= a[p])]
+# we can write   m = new choose{l,r}[! (a[i] <= a[p])]
+# means
