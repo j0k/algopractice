e1c743765f7ddb4a90c2e9e465d5733521931684
j0k <darling.kicks@gmail.com>
2017-09-25 19:30:15 +0300

    classic qsort

---
+++ qsort/qs27.py
@@ -0,0 +1,30 @@
+def qsort(a):
+
+    if len(a)<= 1:
+        return a
+
+    p = int(len(a)/2)
+
+    e = a[p]
+    l,r = [],[]
+
+    for i in range(len(a)):
+        ai = a[i]
+        if i == p:
+            continue
+
+        if a[i]<=e:
+            l += [ai]
+        else:
+            r += [ai]
+
+    return qsort(l) + [e] + qsort(r)
+
+import random
+
+def rp(a):
+    return random.sample(a,len(a))
+
+A = rp (range(20))
+print A
+print qsort(A)
