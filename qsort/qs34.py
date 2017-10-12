8163acfd74bcc3c3a119c22d3223b363d068e05c
j0k <darling.kicks@gmail.com>
2017-10-12 03:31:05 +0300

    quicksort with AML examples

--- qsort/qs34.py
+++ qsort/qs34.py
@@ -1,3 +1,6 @@
+# Airport Pulkovo
+# 12.10.2017 03:14
+
 import random
 
 def qsort(a):
@@ -24,3 +27,31 @@ A = random.sample(A, len(A))
 
 print A
 print qsort(A)
+
+### how it will be in AML:
+###
+###
+# a += [e], where a = l if e <= a[p] else r
+# a += [e] /. a = l if e <= a[p] else r
+
+# aml example
+#p,l,r = (int) len(a)/2, [], []
+#not p == 0
+
+#for e in a:
+#    not e.i == p
+#    a += [e] /. a = l if e <= a[p] else r
+
+#return $f(l) + [a[p]] + $f(r)
+
+###
+#p,l,r = len(a)/2, [], []
+#e in a[^p]
+#  a += [e]
+#    /. a = {e<=a[p]: l, r}
+
+# {e<=a[p]: l, r} += [e] for e in a[^p]
+
+# the same
+# not e.i == p
+# continue if e.i == p
