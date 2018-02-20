9b839e74ad75731cb4f57501d0d288fa81ea12d1
j0k <darling.kicks@gmail.com>
2018-02-20 09:32:49 +0300

    max sequence classic in spagating

---
+++ seq/maxsubseq/mss10.py
@@ -0,0 +1,49 @@
+# 20.02.2018 in spagatin at 151 09:27
+# 09:32 end
+
+import random
+
+
+
+def maxi(a,i,b,j):
+    return (a,i) if a>=b else (b,j)
+
+def mini(a,i,b,j):
+    return (a,i) if a<=b else (b,j)
+
+
+def maxseq(a):
+    if len(a)<=1:
+        return 0
+
+    m,M,mi,Mi,le = a[0], a[0], 0, 0, a[0]
+    t = 1
+    A = []
+    for i,e in enumerate(a):
+        if e == le:
+            continue
+        elif (e>le and t > 0) or (e < le and t < 0):
+            m,mi = mini(e,i,m,mi)
+            M,Mi = maxi(e,i,M,Mi)
+        else:
+            A   += [(M-m,m,mi,M,Mi)]
+            m,mi = mini(e,i,le,i-1)
+            M,Mi = maxi(e,i,le,i-1)
+            t    = t * -1
+        le = e
+
+    A += [(M-m,m,mi,M,Mi)]
+
+    d_A   = list(map(lambda x:x[0], A))
+    i_max = d_A.index(max(d_A))
+
+    (d,m,mi,M,Mi) = A[i_max]
+    l,r = min(mi,Mi), max(mi,Mi) # AML min,max /@ (mi,Mi)
+
+    return (a[l:r+1], d, m,M, mi, Mi)
+
+A = list(range(27))
+A = random.sample(A, len(A))
+
+print(A)
+print(maxseq(A))
